4c038485ff6c80a93005338d2da7b1478b0b6da8
Gumerov Vladislav <gumerov.vlad20034@gmail.com>
2024-11-23 19:37:40 +0700

    update checklist

--- src/components/pages/home/checklist/CheckList.tsx
+++ src/components/pages/home/checklist/CheckList.tsx
@@ -4,29 +4,42 @@ import CheckListItem from './CheckListItem'
 
 import styles from './CheckList.module.scss'
 import { useTypedSelector } from '../../../../hooks/useTypedSelector'
+import { Field, Label, Switch } from '@headlessui/react'
 
 const CheckList: FC = () => {
 	const { checklist } = useTypedSelector(state => state.checklist)
 
 	return (
 		<Container title={'Чек-лист'}>
-			<ul className={styles.checklist}>
-				{checklist.length ? (
-					checklist.map(item => (
-						<CheckListItem color={item.color} text={item.text} />
-					))
-				) : (
-					<li className='text-center text-gray-500'>
-						Пока нет актуальных этапов...
-					</li>
-				)}
-				{/*<CheckListItem color={'success'} text={'Этап 1'}/>*/}
-				{/*<CheckListItem color={'success'} text={'Этап 2'} />*/}
-				{/*<CheckListItem color={'error'} text={'Этап 3'} />*/}
-				{/*<CheckListItem color={'error'} text={'Этап 4'} />*/}
-				{/*<CheckListItem color={'other'} text={'Этап 5'} />*/}
-				{/*<CheckListItem color={'other'} text={'Этап 6'} />*/}
-			</ul>
+			<div className='w-full flex flex-col justify-between'>
+				<div className='w-full'>
+					<ul className={styles.checklist}>
+						{checklist.length ? (
+							checklist.map(item => (
+								<CheckListItem color={item.color} text={item.text} />
+							))
+						) : (
+							<li className='text-center text-gray-500'>
+								Пока нет актуальных этапов...
+							</li>
+						)}
+					</ul>
+				</div>
+				<div className='w-full flex flex-col flex-grow-3 space-y-4 font-oswald text-xl'>
+					<Field className='flex items-center justify-between'>
+						<Label>Заземлено</Label>
+						<Switch className='group inline-flex h-6 w-11 items-center rounded-full bg-gray-200 transition data-[checked]:bg-blue-600'>
+							<span className='size-4 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-6' />
+						</Switch>
+					</Field>
+					<Field className='flex items-center justify-between'>
+						<Label>Заземление убрано</Label>
+						<Switch className='group inline-flex h-6 w-11 items-center rounded-full bg-gray-200 transition data-[checked]:bg-blue-600'>
+							<span className='size-4 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-6' />
+						</Switch>
+					</Field>
+				</div>
+			</div>
 		</Container>
 	)
 }
