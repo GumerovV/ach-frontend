107e9eb08579246236ef2778aadd5e955def34ac
Gumerov Vladislav <gumerov.vlad20034@gmail.com>
2024-11-25 17:00:50 +0700

    create carousel ui

--- src/components/pages/home/violations/Violations.tsx
+++ src/components/pages/home/violations/Violations.tsx
@@ -3,19 +3,62 @@ import Container from '../../../ui/container/Container'
 
 import styles from './Violations.module.scss'
 import ViolationItem from './ViolationItem'
+import Carousel from '../../../ui/carousel/Carousel'
 
 const Violations: FC = () => {
 	return (
 		<Container title={'Подтверждение нарушения'}>
 			<ul className={styles.violations}>
-				<ViolationItem
-					achName={'ACH-7'}
-					violationName={'Название нарушения'}
-					date={'12/03/2024'}
-					time={'19:16:35'}
-					photoPath={''}
-					videoPath={''}
-				/>
+				<Carousel>
+					<ViolationItem
+						achName={'ACH-7'}
+						violationName={'Название нарушения'}
+						date={'12/03/2024'}
+						time={'19:16:35'}
+						photoPath={''}
+						videoPath={''}
+					/>
+					<ViolationItem
+						achName={'ACH-7'}
+						violationName={'Название нарушения'}
+						date={'12/03/2024'}
+						time={'19:16:35'}
+						photoPath={''}
+						videoPath={''}
+					/>
+					<ViolationItem
+						achName={'ACH-7'}
+						violationName={'Название нарушения'}
+						date={'12/03/2024'}
+						time={'19:16:35'}
+						photoPath={''}
+						videoPath={''}
+					/>
+					<ViolationItem
+						achName={'ACH-7'}
+						violationName={'Название нарушения'}
+						date={'12/03/2024'}
+						time={'19:16:35'}
+						photoPath={''}
+						videoPath={''}
+					/>
+					<ViolationItem
+						achName={'ACH-7'}
+						violationName={'Название нарушения'}
+						date={'12/03/2024'}
+						time={'19:16:35'}
+						photoPath={''}
+						videoPath={''}
+					/>
+					<ViolationItem
+						achName={'ACH-7'}
+						violationName={'Название нарушения'}
+						date={'12/03/2024'}
+						time={'19:16:35'}
+						photoPath={''}
+						videoPath={''}
+					/>
+				</Carousel>
 			</ul>
 		</Container>
 	)
---
+++ src/components/ui/carousel/Carousel.tsx
@@ -0,0 +1,50 @@
+import React, { Children, FC, PropsWithChildren, useState } from 'react'
+
+import styles from './Carousel.module.scss'
+import { BiChevronLeft, BiChevronRight } from 'react-icons/bi'
+import { BsDot } from 'react-icons/bs'
+
+const Carousel: FC<PropsWithChildren> = ({ children }) => {
+	const [curIndex, setCurIndex] = useState<number>(0)
+
+	const next = () => {
+		setCurIndex(index =>
+			index === Children.toArray(children).length - 1 ? 0 : index + 1,
+		)
+	}
+
+	const prev = () => {
+		setCurIndex(index =>
+			index === 0 ? Children.toArray(children).length - 1 : index - 1,
+		)
+	}
+
+	return (
+		<div className={styles.carousel}>
+			<div
+				className={styles.items}
+				style={{ transform: `translateX(-${curIndex * 100}%)` }}
+			>
+				{children}
+			</div>
+			<div className={styles.actions}>
+				<button onClick={prev}>
+					<BiChevronLeft />
+				</button>
+				<div className={styles.dots}>
+					{Children.toArray(children).map((_, i) => (
+						<BsDot
+							className={curIndex === i ? styles.active : ''}
+							onClick={() => setCurIndex(i)}
+						/>
+					))}
+				</div>
+				<button onClick={next}>
+					<BiChevronRight />
+				</button>
+			</div>
+		</div>
+	)
+}
+
+export default Carousel
